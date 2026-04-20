89e0e59d4e201f928a3c6f2888b39c927bbd93a8
ashutosh <ashutoshsahu375@gmail.com>
2026-04-20 17:55:31 +0530

    minorchng

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 import os
 
-OLLAMA_MODEL = os.getenv("OLLAMA_MODEL", "llama3")  # ✅ better default
+OLLAMA_MODEL = os.getenv("OLLAMA_MODEL", "llama3 ")  # ✅ better default
 OLLAMA_URL = os.getenv("OLLAMA_URL", "http://localhost:11434/api/generate")
 
 EMBEDDING_MODEL = "all-MiniLM-L6-v2"
